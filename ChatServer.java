ff11aa2ec67474a3e05a4aab93766dd60c3831cb
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-04-20 23:09:26 -0700

    Update and rename StartServer.java to ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
 import java.net.URI;
 
+//Lab report 2 Chat Server
+//If I enter like ".../add-message?s=<string>&user=<string>", the message will be displayed like : "string(user): string(s)"
+
+
 class Counter implements URLHandler {
   int num = 0;
   public String handleRequest(URI url) {
@@ -14,17 +18,16 @@ class Counter implements URLHandler {
       if(url.contain("&user="){
         username = url.substring(url.indexOf("user=")+5, url.length()-1);
       }
-      return username + ", " + message;
+      return username + ": " + message;
     }
     else {
-      return "Don't know what to do with that path!";
+      return "Enter the valid path!";
     }
   }
 }
 class CounterMain {
   public static void main(String[] args) throws IOException {
     int port = Integer.parseInt(args[0]);
-    // We wrote Server; it is a very short class (you can see it in Server.java in lab 2)
     Server.start(port, new Counter());
   }
 }
