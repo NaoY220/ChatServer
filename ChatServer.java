f002ba6c7a91fbe134dbf6226355dba1c5a1e64a
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-04-23 17:30:08 -0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -4,16 +4,15 @@ import java.net.URI;
 //Lab report 2 Chat Server
 //If I enter like ".../add-message?s=<string>&user=<string>", the message will be displayed like : "string(user): string(s)"
 
-class Counter implements URLHandler {
-  int num = 0;
+class Handler implements URLHandler {
   
   public String handleRequest(URI url) {
+    String username = "", message = "";
     System.out.println(url);
     if (url.getPath().equals("/")) {
         return "No request to the server!";
     }
     if (url.getPath().equals("/add-message")){ 
-      String username = "", message = "";
         if(url.getPath().contains("s=")){
             username = url.getPath().substring(url.getPath().indexOf("s=")+2, url.getPath().indexOf("&"));
         }
