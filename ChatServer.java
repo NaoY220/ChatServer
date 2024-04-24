4c1256eb8ab90dfb718862fee4cecc1fb76795cb
NaoY220 <129801469+NaoY220@users.noreply.github.com>
2024-04-23 17:20:08 -0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -10,20 +10,20 @@ class Counter implements URLHandler {
   public String handleRequest(URI url) {
     System.out.println(url);
     if (url.getPath().equals("/")) {
-      return "No request to the server!";
+        return "No request to the server!";
     }
     if (url.getPath().equals("/add-message")){ 
-      String username="", message = "";
-      if(url.getPath().contains("s=")){
-        username = url.getPath().substring(url.getPath().indexOf("s=")+2, url.getPath().indexOf("&"));
-      }
-      if(url.getPath().contains("&user=")){
-        username = url.getPath().substring(url.getPath().indexOf("user=")+5, url.getPath().length()-1);
-      }
-      return username + ": " + message;
+      String username = "", message = "";
+        if(url.getPath().contains("s=")){
+            username = url.getPath().substring(url.getPath().indexOf("s=")+2, url.getPath().indexOf("&"));
+        }
+        if(url.getPath().contains("&user=")){
+            username = url.getPath().substring(url.getPath().indexOf("user=")+5, url.getPath().length()-1);
+        }
+        return username + ": " + message;
     }
     else {
-      return "Enter the valid path!";
+        return "Enter the valid path!";
     }
   }
 }
